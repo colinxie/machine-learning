85c825173a8a3df6d01e3250b7661d7995dc25d3
colinxie <xiezhch12@gmail.com>
2017-11-27 13:15:25 +0800

    Update agent.py

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -23,7 +23,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
-
+        self.trial =0
 
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
@@ -39,10 +39,14 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-        self.epsilon -= 0.05 
+        
+        #self.epsilon -= 0.05 
         if testing is True: 
             self.epsilon = 0.0 
             self.alpha = 0.0
+        else:
+            self.trial +=1
+            self.epsilon = pow(self.alpha,self.trial)#math.exp(-1 * self.trial * self.alpha )
         return None
 
     def build_state(self):
@@ -66,7 +70,7 @@ class LearningAgent(Agent):
         
         # Set 'state' as a tuple of relevant data for the agent        
         # to avoid being unhashable, convert input to string
-        self.state = (str(waypoint), str(inputs['left']), str(inputs['oncoming']), str(inputs['light']))
+        self.state = (waypoint, inputs['left'], inputs['oncoming'], inputs['light'])
         return self.state
 
     
@@ -190,14 +194,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator( env, update_delay= 0.01, log_metrics= True)
+    sim = Simulator( env, update_delay= 0.01, log_metrics= True,optimized =True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test = 10)
+    sim.run(n_test = 20)
 
 
 if __name__ == '__main__':
